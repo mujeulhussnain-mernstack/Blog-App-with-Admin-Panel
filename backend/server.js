6e4cfafb852d1025ddecee411f6cb3423b36eafb
Muje Ul Hussnain <mujeulhussnain@gmail.com>
2026-01-18 15:32:10 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const __dirname = path.resolve();
 
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }))
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(cors({ origin: 'https://blog-app-with-admin-panel-joe3.onrender.com', credentials: true }))
 app.use(cookieParser())
 
 app.use('/api/v1/user', userRoute)
